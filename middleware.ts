e045999aaed1fa73d536d8a0d3e57063b35d7862
matthtony <127679282+matthtony@users.noreply.github.com>
2025-07-28 12:09:05 +0800

    chore: disable auth redirects

--- middleware.ts
+++ middleware.ts
@@ -8,28 +8,7 @@ export async function middleware(request: NextRequest) {
   if (i18nResult) return i18nResult
 
   try {
-    const { supabase, response } = createClient(request)
-
-    const session = await supabase.auth.getSession()
-
-    const redirectToChat = session && request.nextUrl.pathname === "/"
-
-    if (redirectToChat) {
-      const { data: homeWorkspace, error } = await supabase
-        .from("workspaces")
-        .select("*")
-        .eq("user_id", session.data.session?.user.id)
-        .eq("is_home", true)
-        .single()
-
-      if (!homeWorkspace) {
-        throw new Error(error?.message)
-      }
-
-      return NextResponse.redirect(
-        new URL(`/${homeWorkspace.id}/chat`, request.url)
-      )
-    }
+    const { response } = createClient(request)
 
     return response
   } catch (e) {
